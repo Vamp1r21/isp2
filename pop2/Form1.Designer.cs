59a468b8ac290d0f421c5414021addfc7ac21d29
Vampir <nik.aleks.ru@yandex.ru>
2022-05-25 18:23:50 +0300

    Добавьте файлы проекта.

---
+++ pop2/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace pop2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(532, 347);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(563, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 62);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(706, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 378);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Back";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(506, 378);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Next";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(987, 567);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ pop2/Form1.cs
@@ -0,0 +1,99 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pop2
+{
+    public partial class Form1 : Form
+    {
+        string str = "server=localhost; user=root; password= 1234; database= chernovik; port= 3306";
+        int s = 0;
+        int f = 0;
+        string sql = "";
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Hide();
+            label2.Hide();
+            comboBox1.Hide();
+            comboBox1.Items.Clear();
+            MySqlConnection connection = new MySqlConnection(str);
+            connection.Open();
+            sql = "Select * from type_material";
+            MySqlDataAdapter adapter = new MySqlDataAdapter(sql, connection);
+            DataTable ds = new DataTable();
+            adapter.Fill(ds);
+            for(int i=0;i<ds.Rows.Count; i++)
+            {
+                comboBox1.Items.Add(ds.Rows[i]["Name"]);
+            }
+        }
+
+        public void tabl(string sql)
+        {
+            MySqlConnection connection = new MySqlConnection(str);
+            connection.Open();
+            MySqlDataAdapter adapter = new MySqlDataAdapter(sql, connection);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Hide();
+            s = 0;
+            MySqlConnection connection = new MySqlConnection(str);
+            sql = $"Select * from materials";
+            comboBox1.Show();
+            tabl(sql + $"  Limit { s},{ s + 15}");
+            sql = "Select Count(ID) from materials";
+            MySqlDataAdapter adapter = new MySqlDataAdapter(sql, connection);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            f = Int32.Parse(ds.Tables[0].Rows[0].ItemArray[0].ToString());
+            if (f > 15) label2.Show();
+            sql = $"Select * from materials";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            s = 0;
+            label1.Hide();
+            MySqlConnection connection = new MySqlConnection(str);
+            sql = "Select count(materials.ID) from materials inner join type_material on materials.idmateria=type_material.ID and " +
+                $"type_material.Name='{comboBox1.Text}'";
+            MySqlDataAdapter adapter = new MySqlDataAdapter(sql, connection);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            f = Int32.Parse(ds.Tables[0].Rows[0].ItemArray[0].ToString());
+            if (f < 15) label2.Hide();
+            else label2.Show();
+            sql = $"Select * from materials inner join type_material on materials.idmateria=type_material.ID and " +
+                $"type_material.Name='{comboBox1.Text}'";
+            tabl(sql+ $"  Limit { s},{ s + 15}");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            s += 15;
+            label1.Show();
+            tabl(sql + $"  Limit { s},{ s + 15}");
+            if (f < s + 15) label2.Hide();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            s -= 15;
+            label2.Show();
+            tabl(sql + $"  Limit { s},{ s + 15}");
+            if (s==0) label1.Hide();
+        }
+    }
+}
